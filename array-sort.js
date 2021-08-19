411a8f5f9db95582d6582659f31962dde82b856d
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-19 08:10:39 +0600

    add js array string method

---
+++ array-sort.js
@@ -0,0 +1,13 @@
+// signal digit sort 
+const numbers = [1, 4, 5, 8, 2 , 9, 6, 7];
+const sortedNumbers = numbers.sort();
+
+//duble digit sort 
+const bigNumbers = [12,670, 450,9,192,34,54,23];
+const sortedBigNumbers = bigNumbers.sort(function(a,b){
+    return a-b;
+});
+//word sorting
+const friends = ['korim','khorim','sumon','ratul','bikrom','babul','akib','afif','afie'];
+const sortedFriends = friends.sort();
+console.log( sortedFriends);
