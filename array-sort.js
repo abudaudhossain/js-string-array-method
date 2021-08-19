34d11e30f4257b3454e7fc17ed0fe261f071c42e
abudaudhossain <abudaudhossainsumon@gmail.com>
2021-08-19 08:16:23 +0600

    add array sort method

--- array-sort.js
+++ array-sort.js
@@ -1,7 +1,8 @@
 // signal digit sort 
 const numbers = [1, 4, 5, 8, 2 , 9, 6, 7];
 const sortedNumbers = numbers.sort();
-
+const reverseSortNumbers = sortedNumbers.reverse();
+console.log(reverseSortNumbers);
 //duble digit sort 
 const bigNumbers = [12,670, 450,9,192,34,54,23];
 const sortedBigNumbers = bigNumbers.sort(function(a,b){
@@ -9,5 +10,6 @@ const sortedBigNumbers = bigNumbers.sort(function(a,b){
 });
 //word sorting
 const friends = ['korim','khorim','sumon','ratul','bikrom','babul','akib','afif','afie'];
-const sortedFriends = friends.sort();
-console.log( sortedFriends);
+// const sortedFriends = friends.sort();
+const reverseSortFriends = friends.reverse();
+console.log(reverseSortFriends);
